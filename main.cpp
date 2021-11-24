82d3b7ea2c69b0726bcffb3bec692b6a637fa624
Mei-Ling Laures <mlaures@caltech.edu>
2021-11-24 11:28:48 -0800

    Rename buffer and memory classes

--- main.cpp
+++ main.cpp
@@ -6,17 +6,17 @@
 #include "r3.hpp"
 
 // ask class for buffers and hand them back
-void make_vis(Recycle<int>& r3) {
+void make_vis(recycle_memory<int>& r3) {
     // make 5 different integers
     for (int i = 0; i < 5; i++) {
-        Reuse<int>* r = r3.new_ptr();
+        reuseable_buffer<int>* r = r3.new_ptr();
         r->ptr = &i;
         r3.queue_ptr(r);
     }
 }
 
 // read from visibility and write to disk (just write to terminal right now)
-void read_vis(Recycle<int>& r3, Reuse<int>* vis) {
+void read_vis(recycle_memory<int>& r3, reuseable_buffer<int>* vis) {
     //std::ofstream file;
     //file.open("test.txt");
     std::cout << *vis->ptr << std::endl;
@@ -25,14 +25,14 @@ void read_vis(Recycle<int>& r3, Reuse<int>* vis) {
 }
 
 // ask for visibility and make thread to read (need to do this continuously??)
-void change_vis(Recycle<int>& r3) {
+void change_vis(recycle_memory<int>& r3) {
     while (true) { // what is the condition that needs to be here? will this need to be a condition?
-        Reuse<int> *vis = r3.start_operation();
+        reuseable_buffer<int> *vis = r3.start_operation();
         int i = *vis->ptr;
         *vis->ptr = i+1;
         // read_vis should actually be the type of vis's pointer
-        Recycle<int>::func f = read_vis;
-        std::vector<Recycle<int>::func> v {f};
+        recycle_memory<int>::func f = read_vis;
+        std::vector<recycle_memory<int>::func> v {f};
         r3.end_operation(vis, &v);
     }
 }
@@ -41,7 +41,7 @@ void change_vis(Recycle<int>& r3) {
 int main(int argc, char** argv) {
     // make the class
     std::vector<size_t> shape = std::vector<size_t>(1, 1);
-    Recycle<int> r3 = Recycle<int>(shape, 1); // max number does nothing right now
+    recycle_memory<int> r3 = recycle_memory<int>(shape, 1); // max number does nothing right now
 
     // make a thread to be making visibilities
     std::thread making (make_vis, std::ref(r3));
