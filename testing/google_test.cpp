71885398056233c5a8d2bbe57240f5a08a700749
Mei-Ling Laures <mlaures@caltech.edu>
2022-01-20 12:55:56 -0800

    Add float duplicates of int tests

--- testing/google_test.cpp
+++ testing/google_test.cpp
@@ -2,13 +2,13 @@
 #include "test_class.hpp"
 #include "../r3.hpp"
 
-TEST(BasicFunctions, MakeRecycleMemory) {
+TEST(BasicFunctions, IntMakeRecycleMemory) {
     int max = 1;
     std::vector<size_t> shape = std::vector<size_t>(1, 1);
     unit_test::make_recycle_memory<int>(shape, max);
 }
 
-TEST(BasicFunctions, TakeWithFill) {
+TEST(BasicFunctions, IntTakeWithFill) {
     int max = 1;
     std::vector<size_t> shape = std::vector<size_t>(1, 1);
     std::shared_ptr<recycle_memory<int>> r3 = 
@@ -16,7 +16,7 @@ TEST(BasicFunctions, TakeWithFill) {
     unit_test::take_one_buffer_from_fill<int>(r3, shape, max);
 }
 
-TEST(BasicFunctions, CheckBufferDestruction) {
+TEST(BasicFunctions, IntCheckBufferDestruction) {
     int max = 1;
     std::vector<size_t> shape = std::vector<size_t>(1, 1);
     std::shared_ptr<recycle_memory<int>> r3 = 
@@ -25,7 +25,7 @@ TEST(BasicFunctions, CheckBufferDestruction) {
     unit_test::check_buffer_destruction<int>(r3, max);
 }
 
-TEST(BasicFunctions, ChangeBuffers) {
+TEST(BasicFunctions, IntChangeBuffers) {
     int max = 2;
     std::vector<size_t> shape = std::vector<size_t>(1, 1);
     std::shared_ptr<recycle_memory<int>> r3 = 
@@ -33,7 +33,7 @@ TEST(BasicFunctions, ChangeBuffers) {
     unit_test::change_one_buffer<int>(r3, max, 5);
 }
 
-TEST(BasicFunctions, MultiChangeBuffers) {
+TEST(BasicFunctions, IntMultiChangeBuffers) {
     int max = 2;
     std::vector<size_t> shape = std::vector<size_t>(1, 1);
     std::shared_ptr<recycle_memory<int>> r3 = 
@@ -41,7 +41,7 @@ TEST(BasicFunctions, MultiChangeBuffers) {
     unit_test::multi_change_buffer<int>(r3, max, 5, 2);
 }
 
-TEST(BasicFunctions, SetBufferArray) {
+TEST(BasicFunctions, IntSetBufferArray) {
     int max = 2;
     std::vector<size_t> shape = std::vector<size_t>(2, 2);
     std::shared_ptr<recycle_memory<int>> r3 = 
@@ -49,7 +49,7 @@ TEST(BasicFunctions, SetBufferArray) {
     unit_test::set_buffer_ptr_array<int>(r3);
 }
 
-TEST(BasicFunctions, QueueBuffer) {
+TEST(BasicFunctions, IntQueueBuffer) {
     int max = 1;
     std::vector<size_t> shape = std::vector<size_t>(1, 1);
     std::shared_ptr<recycle_memory<int>> r3 = 
@@ -57,7 +57,62 @@ TEST(BasicFunctions, QueueBuffer) {
     unit_test::queue_buffer_from_fill<int>(r3, max, 5);
 }
 
-TEST(Concurrency, ThreadSeesBufferChanges) {
+TEST(BasicFunctions, FloatMakeRecycleMemory) {
+    int max = 1;
+    std::vector<size_t> shape = std::vector<size_t>(1, 1);
+    unit_test::make_recycle_memory<float>(shape, max);
+}
+
+TEST(BasicFunctions, FloatTakeWithFill) {
+    int max = 1;
+    std::vector<size_t> shape = std::vector<size_t>(1, 1);
+    std::shared_ptr<recycle_memory<float>> r3 = 
+        std::make_shared<recycle_memory<float>>(shape, max); 
+    unit_test::take_one_buffer_from_fill<float>(r3, shape, max);
+}
+
+TEST(BasicFunctions, FloatCheckBufferDestruction) {
+    int max = 1;
+    std::vector<size_t> shape = std::vector<size_t>(1, 1);
+    std::shared_ptr<recycle_memory<float>> r3 = 
+        std::make_shared<recycle_memory<float>>(shape, max); 
+    // unit_test::take_one_buffer_from_fill(r3, shape, max);
+    unit_test::check_buffer_destruction<float>(r3, max);
+}
+
+TEST(BasicFunctions, FloatChangeBuffers) {
+    int max = 2;
+    std::vector<size_t> shape = std::vector<size_t>(1, 1);
+    std::shared_ptr<recycle_memory<float>> r3 = 
+        std::make_shared<recycle_memory<float>>(shape, max); 
+    unit_test::change_one_buffer<float>(r3, max, 5.0);
+}
+
+TEST(BasicFunctions, FloatMultiChangeBuffers) {
+    int max = 2;
+    std::vector<size_t> shape = std::vector<size_t>(1, 1);
+    std::shared_ptr<recycle_memory<float>> r3 = 
+        std::make_shared<recycle_memory<float>>(shape, max); 
+    unit_test::multi_change_buffer<float>(r3, max, 5.0, 2.0);
+}
+
+TEST(BasicFunctions, FloatSetBufferArray) {
+    int max = 2;
+    std::vector<size_t> shape = std::vector<size_t>(2, 2);
+    std::shared_ptr<recycle_memory<float>> r3 = 
+        std::make_shared<recycle_memory<float>>(shape, max); 
+    unit_test::set_buffer_ptr_array<float>(r3);
+}
+
+TEST(BasicFunctions, FloatQueueBuffer) {
+    int max = 1;
+    std::vector<size_t> shape = std::vector<size_t>(1, 1);
+    std::shared_ptr<recycle_memory<float>> r3 = 
+        std::make_shared<recycle_memory<float>>(shape, max); 
+    unit_test::queue_buffer_from_fill<float>(r3, max, 5.0);
+}
+
+TEST(Concurrency, ThreadIntSeesBufferChanges) {
     int max = 1;
     std::vector<size_t> shape = std::vector<size_t>(1, 1);
     std::shared_ptr<recycle_memory<int>> r3 = 
@@ -65,7 +120,15 @@ TEST(Concurrency, ThreadSeesBufferChanges) {
     unit_test::change_buffer_threaded<int>(r3, 5);
 }
 
-TEST(Concurrency, ThreadMultiBufferChange) {
+TEST(Concurrency, ThreadFloatSeesBufferChanges) {
+    int max = 1;
+    std::vector<size_t> shape = std::vector<size_t>(1, 1);
+    std::shared_ptr<recycle_memory<float>> r3 = 
+        std::make_shared<recycle_memory<float>>(shape, max); 
+    unit_test::change_buffer_threaded<float>(r3, 5.0);
+}
+
+TEST(Concurrency, ThreadIntMultiBufferChange) {
     int max = 1;
     std::vector<size_t> shape = std::vector<size_t>(1, 1);
     std::shared_ptr<recycle_memory<int>> r3 = 
@@ -74,7 +137,16 @@ TEST(Concurrency, ThreadMultiBufferChange) {
     unit_test::multi_change_buffer_threaded<int>(r3, data);
 }
 
-TEST(Concurrency, ThreadWaitsForFill) {
+TEST(Concurrency, ThreadFloatMultiBufferChange) {
+    int max = 1;
+    std::vector<size_t> shape = std::vector<size_t>(1, 1);
+    std::shared_ptr<recycle_memory<float>> r3 = 
+        std::make_shared<recycle_memory<float>>(shape, max); 
+    float data[3] = {5.0, 9.0, 12.0};
+    unit_test::multi_change_buffer_threaded<float>(r3, data);
+}
+
+TEST(Concurrency, ThreadIntWaitsForFill) {
     int max = 1;
     std::vector<size_t> shape = std::vector<size_t>(1, 1);
     std::shared_ptr<recycle_memory<int>> r3 = 
@@ -82,7 +154,15 @@ TEST(Concurrency, ThreadWaitsForFill) {
     unit_test::wait_take_from_fill_threaded<int>(r3, max, 5);
 }
 
-TEST(Concurrency, ThreadWaitsForQueue) {
+TEST(Concurrency, ThreadFloatWaitsForFill) {
+    int max = 1;
+    std::vector<size_t> shape = std::vector<size_t>(1, 1);
+    std::shared_ptr<recycle_memory<float>> r3 = 
+        std::make_shared<recycle_memory<float>>(shape, max); 
+    unit_test::wait_take_from_fill_threaded<float>(r3, max, 5.0);
+}
+
+TEST(Concurrency, ThreadIntWaitsForQueue) {
     int max = 1;
     std::vector<size_t> shape = std::vector<size_t>(1, 1);
     std::shared_ptr<recycle_memory<int>> r3 = 
@@ -90,12 +170,28 @@ TEST(Concurrency, ThreadWaitsForQueue) {
     unit_test::buffer_from_empty_queue_threaded<int>(r3, max, 5);
 }
 
-TEST(Concurrency, ThreadMultiWaitsForFill) {
+TEST(Concurrency, ThreadFloatWaitsForQueue) {
+    int max = 1;
+    std::vector<size_t> shape = std::vector<size_t>(1, 1);
+    std::shared_ptr<recycle_memory<float>> r3 = 
+        std::make_shared<recycle_memory<float>>(shape, max); 
+    unit_test::buffer_from_empty_queue_threaded<float>(r3, max, 5.0);
+}
+
+TEST(Concurrency, ThreadIntMultiWaitsForFill) {
     int max = 3;
     std::vector<size_t> shape = std::vector<size_t>(1, 1);
     std::shared_ptr<recycle_memory<int>> r3 = 
         std::make_shared<recycle_memory<int>>(shape, max); 
     unit_test::wait_multi_take_from_fill_threaded<int>(r3, max, 5);
+}
+
+TEST(Concurrency, ThreadFloatMultiWaitsForFill) {
+    int max = 3;
+    std::vector<size_t> shape = std::vector<size_t>(1, 1);
+    std::shared_ptr<recycle_memory<float>> r3 = 
+        std::make_shared<recycle_memory<float>>(shape, max); 
+    unit_test::wait_multi_take_from_fill_threaded<float>(r3, max, 5.0);
 }
 
 TEST(Concurrency, ThreadWatchesBuffer) {
