09ac1cc182c8159285554986403b8c0f8c4be913
Mei-Ling Laures <mlaures@caltech.edu>
2022-08-12 14:25:47 -0700

    Separate BasicFunctions into buffer/library template fixtures

---
+++ testing/test_suites/library_test.cpp
@@ -0,0 +1,40 @@
+#include "gtest/gtest.h"
+#include "test_class.hpp"
+#include "../lender.hpp"
+#include <complex>
+#include <numeric>
+
+typedef std::complex<float> fcomplex;
+using namespace std::complex_literals;
+
+template <typename T>
+class LibraryTest : public testing::Test {
+    protected:
+        void SetUp() override {
+            max = 2;
+            shape = std::vector<size_t>(1, 1);
+            recycler = std::make_shared<library<T>>(shape, max);
+        }
+
+        // void TearDown() override {}
+
+    public:
+        int max;
+        std::vector<size_t> shape;
+        std::shared_ptr<library<T>> recycler = nullptr;
+};
+
+using Primitives = ::testing::Types<int, float, fcomplex>;
+TYPED_TEST_SUITE(LibraryTest, Primitives);
+
+TYPED_TEST(LibraryTest, MakeRecycleMemory) {
+    unit_test::make_recycle_memory<TypeParam>(this->shape, this->max);
+}
+
+TYPED_TEST(LibraryTest, QueueBuffer) {
+    unit_test::queue_buffer_from_fill<TypeParam>(this->recycler, this->max, 5);
+}
+
+TYPED_TEST(LibraryTest, DecOperate) {
+    unit_test::dec_operate_queue<TypeParam>(this->recycler);
+}
